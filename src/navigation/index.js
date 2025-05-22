ae56ca0dcb01565e8b4bce9b99fba601f59427ff
Handy1738 <handyaccess1738@gmail.com>
2025-05-23 09:30:49 +1000

    Create index.js

---
+++ src/navigation/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import { NavigationContainer } from '@react-navigation/native';
+import { createStackNavigator } from '@react-navigation/stack';
+import TabNavigator from './TabNavigator';
+import OnboardingScreen from '../screens/OnboardingScreen';
+import AuthScreen from '../screens/AuthScreen';
+
+const Stack = createStackNavigator();
+
+const Navigation = () => {
+  const isAuthenticated = false;
+  const hasCompletedOnboarding = false;
+
+  return (
+    <NavigationContainer>
+      <Stack.Navigator screenOptions={{ headerShown: false }}>
+        {!hasCompletedOnboarding ? (
+          <Stack.Screen name="Onboarding" component={OnboardingScreen} />
+        ) : !isAuthenticated ? (
+          <Stack.Screen name="Auth" component={AuthScreen} />
+        ) : (
+          <Stack.Screen name="Main" component={TabNavigator} />
+        )}
+      </Stack.Navigator>
+    </NavigationContainer>
+  );
+};
+
+export default Navigation;
